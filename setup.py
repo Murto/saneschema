e0fa780b1a7ebf704192b8ef6caf450945a8452a
Murray Steele <contact@murto.dev>
2019-08-23 20:41:58 +0100

    Add setup script for distribution

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name='saneschema',
+  version='2.0',
+  scripts=['saneschema.py'],
+  author='Murray Steele',
+  author_email='contact@murto.dev',
+  description='A simple JSON schema package',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/murto/saneschema',
+  packages=setuptools.find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT LICENSE',
+    'Operating System :: OS Independent',
+  ],
+)
