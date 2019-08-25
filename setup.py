035606edb96e000fb65b7984ca389ffa460a4cc6
Murray Steele <contact@murto.dev>
2019-08-25 21:56:06 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
   name='saneschema',
-  version='2.1',
+  version='2.2',
   scripts=['saneschema.py'],
   author='Murray Steele',
   author_email='contact@murto.dev',
