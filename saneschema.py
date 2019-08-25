6a72763e6480ac47c7ff292f21291c2e52d186ca
Murray Steele <contact@murto.dev>
2019-08-25 17:31:52 +0100

    Add docstrings and remove duplicate function

--- saneschema.py
+++ saneschema.py
@@ -4,9 +4,12 @@ import logging
 import json
 import re
 
+
+# Error class used for all exceptions from the package
 class SchemaCheckError(BaseException):
   
   def __init__(self, msg):
+    """Initialise with a given message."""
     super().__init__(msg)
     self.msg = msg
 
@@ -22,6 +25,7 @@ class Schema:
   NULL_TYPE    = type(json.loads('null'))
 
   def __init__(self, path):
+    """Read the JSON file with the given path as the schema."""
     self.__logger = logging.getLogger(__name__)
     try:
       with open(path, 'r') as f:
@@ -37,9 +41,11 @@ class Schema:
       raise SchemaCheckError('An Unknown error occurred. Please contact the package maintainer')
 
   def check(self, unchecked):
+    """Throw a SchemaCheckError if the given unchecked JSON data does not match the schema."""
     self.__check(self._schema, unchecked)
 
   def __check(self, checked, unchecked):
+    """Throw a SchemaCheckError if the given unchecked JSON data does not match the given checked schema"""
     if (type(checked) == Schema.OBJECT_TYPE):
       self.__check_object(checked, unchecked)
     elif (type(checked) == Schema.ARRAY_TYPE):
@@ -51,6 +57,7 @@ class Schema:
       raise SchemaCheckError('Unexpected type')
 
   def __check_string(self, checked, unchecked):
+    """Throw a SchemaCheckError if the unchecked JSON data does not match the JEX"""
     OBJECT_WILDCARD  = '$object'
     ARRAY_WILDCARD   = '$array'
     STRING_WILDCARD  = '$string'
@@ -96,6 +103,7 @@ class Schema:
         raise SchemaCheckError('Error decoding JSON schema')
 
   def __check_object(self, checked, unchecked):
+    """Throw a SchemaCheckError if the unchecked JSON data does not match the checked JSON object"""
     if (type(unchecked) != Schema.OBJECT_TYPE):
       self.__logger.error('Object expected')
       raise SchemaCheckError('Object expected')
@@ -110,6 +118,7 @@ class Schema:
         raise
 
   def __check_array(self, checked, unchecked):
+    """Throw a SchemaCheckError if the unchecked JSON data does not match the checked JSON array"""
     if (type(unchecked) != Schema.ARRAY_TYPE):
       self.__logger.error('Array Expected')
     for item in unchecked:
@@ -123,49 +132,3 @@ class Schema:
       if (not item_checked):
         self.__logger.error('Array item does not fit any item schema')
         raise SchemaCheckError('Array item does not fit any item schema')
-
-
-  def __check_string(self, checked, unchecked):
-    OBJECT_WILDCARD  = '$object'
-    ARRAY_WILDCARD   = '$array'
-    STRING_WILDCARD  = '$string'
-    NUMBER_WILDCARD  = '$number'
-    BOOLEAN_WILDCARD = '$boolean'
-    NULL_WILDCARD    = '$null'
-    if (checked == OBJECT_WILDCARD):
-      if (type(unchecked) != Schema.OBJECT_TYPE):
-        self.__logger.error('Objected expected')
-        raise SchemaCheckError('Object expected')
-      return
-    elif (checked == ARRAY_WILDCARD):
-      if (type(unchecked) != Schema.ARRAY_TYPE):
-        self.__logger.error('Array expected')
-        raise SchemaCheckError('Array expected')
-      return
-    elif (checked == STRING_WILDCARD):
-      if (type(unchecked) != Schema.STRING_TYPE):
-        self.__logger.error('String expected')
-        raise SchemaCheckError('String expected')
-      return
-    elif (checked == NUMBER_WILDCARD):
-      if (not type(unchecked) in (Schema.INT_TYPE, Schema.FLOAT_TYPE)):
-        self.__logger.error('Number expected')
-        raise SchemaCheckError('Number expected')
-      return
-    elif (checked == BOOLEAN_WILDCARD):
-      if (type(unchecked) != Schema.BOOLEAN_TYPE):
-        self.__logger.error('Boolean expected')
-        raise SchemaCheckError('Boolean expected')
-      return
-    elif (checked == NULL_WILDCARD):
-      if (type(unchecked) != Schema.NULL_TYPE):
-        self.__logger.error('Null expected')
-        raise SchemaCheckError('Null expected')
-      return
-    else:
-      try:
-        checked_json = json.loads(checked)
-        self.__check(checked_json, unchecked)
-      except json.JSONDecodeError:
-        self.__logger.error('Error decoding JSON schema')
-        raise SchemaCheckError('Error decoding JSON schema')
