56ae4a9047a2bd2d237fd9f4f69b0b811967c74e
Murray Steele <contact@murto.dev>
2019-08-29 12:21:06 +0100

    Add geoschema GeometryCollection schema

---
+++ tests/test_geojson.py
@@ -0,0 +1,25 @@
+from .context import saneschema
+
+import json
+import os
+import unittest
+
+
+
+DIR = os.path.dirname(os.path.abspath(__file__)) 
+
+class TestGeoJSON(unittest.TestCase):
+
+  def test_geojson(self):
+    SCHEMA_FILE='geo_schema.json'
+    UNCHECKED_FILE='geo.json'
+    self.do_check(SCHEMA_FILE, UNCHECKED_FILE)
+
+  def do_check(self, schema_file, unchecked_file):
+    SCHEMA_PATH = os.path.join(DIR, schema_file)
+    UNCHECKED_PATH = os.path.join(DIR, unchecked_file)
+    schema = saneschema.Schema(SCHEMA_PATH)
+    with open(UNCHECKED_PATH, 'r') as f:
+      unchecked = json.load(f)
+      schema.check(unchecked)
+
